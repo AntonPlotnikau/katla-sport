e3556716cf3b1b87b59a73c29d2afab4d27bd207
Anton Plotnikau <anton.plotnikau@gmail.com>
2018-06-17 14:23:38 +0300

    Merge step8. Add Id, Name, Code and Deleted columns to HiveSectionListComponent.

--- KatlaSportNg/src/app/hive-management/models/hive-section-list-item.ts
+++ KatlaSportNg/src/app/hive-management/models/hive-section-list-item.ts
@@ -1,4 +1,11 @@
 export class HiveSectionListItem {
     constructor(
+        public id: number,
+
+        public name: string,
+        
+        public code: string,
+        
+        public isDeleted: boolean
     ) { }
 }
